a66ab79909e3cd9196b021dd51b66bcd3818984c
flaviogf <flavio.fernandes6@gmail.com>
2021-05-25 15:33:57 -0300

    refactor: removing structs and imports

--- internal/planets/addplanet_test.go
+++ internal/planets/addplanet_test.go
@@ -2,7 +2,6 @@ package planets
 
 import (
 	"context"
-	"errors"
 	"testing"
 )
 
@@ -52,32 +51,4 @@ func TestAddPlanetShouldReturnErrPlanetNotFoundWhenServiceFails(t *testing.T) {
 	if err != ErrPlanetNotFound {
 		t.Errorf("Expected: %v, Got: %v", ErrPlanetNotFound, err)
 	}
-}
-
-type SuccessRepository struct {
-}
-
-func (r SuccessRepository) Add(ctx context.Context, planet Planet) error {
-	return nil
-}
-
-type FailureRepository struct {
-}
-
-func (r FailureRepository) Add(ctx context.Context, planet Planet) error {
-	return errors.New("something goes wrong")
-}
-
-type SuccessService struct {
-}
-
-func (s SuccessService) CountMovies(ctx context.Context, planetName string) (int, error) {
-	return 0, nil
-}
-
-type FailureService struct {
-}
-
-func (s FailureService) CountMovies(ctx context.Context, planetName string) (int, error) {
-	return 0, errors.New("something goes wrong")
 }
