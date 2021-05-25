84fa837755a1b1c835205387903babf2af535af3
flaviogf <flavio.fernandes6@gmail.com>
2021-05-25 15:33:36 -0300

    refactor: creating plante_test file

---
+++ internal/planets/planet_test.go
@@ -0,0 +1,42 @@
+package planets
+
+import (
+	"context"
+	"errors"
+)
+
+type SuccessRepository struct {
+}
+
+func (r SuccessRepository) Add(ctx context.Context, planet Planet) error {
+	return nil
+}
+
+func (r SuccessRepository) GetAll(ctx context.Context) ([]Planet, error) {
+	return []Planet{{"XXXX", "Tatooine", "Hot", "Desert", 5}}, nil
+}
+
+type FailureRepository struct {
+}
+
+func (r FailureRepository) Add(ctx context.Context, planet Planet) error {
+	return errors.New("something goes wrong")
+}
+
+func (r FailureRepository) GetAll(ctx context.Context) ([]Planet, error) {
+	return []Planet{}, errors.New("something goes wrong")
+}
+
+type SuccessService struct {
+}
+
+func (s SuccessService) CountMovies(ctx context.Context, planetName string) (int, error) {
+	return 0, nil
+}
+
+type FailureService struct {
+}
+
+func (s FailureService) CountMovies(ctx context.Context, planetName string) (int, error) {
+	return 0, errors.New("something goes wrong")
+}
